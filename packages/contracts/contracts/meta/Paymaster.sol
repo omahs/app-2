cb1590eb74f546943081dee1f3a5d80007bda126
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2022-02-11 19:01:52 +0100

    remove inheritance

--- packages/contracts/contracts/meta/Paymaster.sol
+++ packages/contracts/contracts/meta/Paymaster.sol
@@ -6,28 +6,15 @@ pragma solidity 0.8.10;
 
 import "@opengsn/contracts/src/BasePaymaster.sol";
 
-// accept everything.
-// this paymaster accepts any request.
-//
-// NOTE: Do NOT use this contract on a mainnet: it accepts anything, so anyone can "grief" it and drain its account
-contract AcceptEverythingPaymaster is BasePaymaster {
+contract Paymaster is BasePaymaster  {
+
+    bool public useSenderWhitelist;
+    bool public useTargetWhitelist;
+    mapping (address=>bool) public senderWhitelist;
+    mapping (address=>bool) public targetWhitelist;
 
     function versionPaymaster() external view override virtual returns (string memory){
         return "2.2.3+opengsn.accepteverything.ipaymaster";
-    }
-
-    function preRelayedCall(
-        GsnTypes.RelayRequest calldata relayRequest,
-        bytes calldata signature,
-        bytes calldata approvalData,
-        uint256 maxPossibleGas
-    )
-    external
-    override
-    virtual
-    returns (bytes memory context, bool revertOnRecipientRevert) {
-        (relayRequest, signature, approvalData, maxPossibleGas);
-        return ("", false);
     }
 
     function postRelayedCall(
@@ -38,18 +25,7 @@ contract AcceptEverythingPaymaster is BasePaymaster {
     ) external override virtual {
         (context, success, gasUseWithoutPost, relayData);
     }
-}
-
-///a sample paymaster that has whitelists for senders and targets.
-/// - if at least one sender is whitelisted, then ONLY whitelisted senders are allowed.
-/// - if at least one target is whitelisted, then ONLY whitelisted targets are allowed.
-contract WhitelistPaymaster is AcceptEverythingPaymaster {
-
-    bool public useSenderWhitelist;
-    bool public useTargetWhitelist;
-    mapping (address=>bool) public senderWhitelist;
-    mapping (address=>bool) public targetWhitelist;
-
+    
     function whitelistSender(address sender) public onlyOwner {
         senderWhitelist[sender]=true;
         useSenderWhitelist = true;
