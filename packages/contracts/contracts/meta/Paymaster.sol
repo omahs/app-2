ce371db952c50b08e81ad8ff58c5e25152cdabf6
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2022-02-16 15:17:20 +0100

    use ACL and UUPS

--- packages/contracts/contracts/meta/Paymaster.sol
+++ packages/contracts/contracts/meta/Paymaster.sol
@@ -4,18 +4,34 @@
 
 pragma solidity 0.8.10;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@opengsn/contracts/src/BasePaymaster.sol";
+import "./../core/acl/ACL.sol";
 
-contract Paymaster is BasePaymaster  {
+/// @title The GSN paymaster paying for meta transaction
+/// @author Michael Heuer - Aragon Association - 2022
+/// @notice This contract pays for meta transactions from and to sponsored addresses
+contract Paymaster is BasePaymaster, Initializable, UUPSUpgradeable, ACL {
 
-    bool public useSenderWhitelist;
-    bool public useTargetWhitelist;
-    mapping (address=>bool) public senderWhitelist;
-    mapping (address=>bool) public targetWhitelist;
+    bytes32 public constant PAYMASTER_UPGRADE_ROLE = keccak256("PAYMASTER_UPGRADE_ROLE");
+    bytes32 public constant PAYMASTER_SPONSORED_ROLE = keccak256("PAYMASTER_SPONSORED_ROLE");
 
+    string private constant ERROR_NOT_SPONSORED = "ERROR_NOT_SPONSORED";
+    string private constant ERROR_APPROVAL_DATA_LENGTH_INVALID = "ERROR_APPROVAL_DATA_LENGTH_INVALID";
+
+    /// @dev Used for GSN IPaymaster compatability
     function versionPaymaster() external view override virtual returns (string memory){
-        return "2.2.3+opengsn.accepteverything.ipaymaster";
+        return "0.0.1";
     }
+
+    /// @dev Used for UUPS upgradability pattern
+    function initialize(address initialOwner) external initializer {
+        ACL.initACL(initialOwner);
+    }
+
+    /// @dev Used to check the permissions within the upgradability pattern implementation of OZ
+    function _authorizeUpgrade(address) internal virtual override auth(address(this), PAYMASTER_UPGRADE_ROLE) {}
 
     function postRelayedCall(
         bytes calldata context,
@@ -24,15 +40,6 @@ contract Paymaster is BasePaymaster  {
         GsnTypes.RelayData calldata relayData
     ) external override virtual {
         (context, success, gasUseWithoutPost, relayData);
-    }
-    
-    function whitelistSender(address sender) public onlyOwner {
-        senderWhitelist[sender]=true;
-        useSenderWhitelist = true;
-    }
-    function whitelistTarget(address target) public onlyOwner {
-        targetWhitelist[target]=true;
-        useTargetWhitelist = true;
     }
 
     function preRelayedCall(
@@ -46,15 +53,19 @@ contract Paymaster is BasePaymaster  {
     virtual
     returns (bytes memory context, bool revertOnRecipientRevert) {
         (signature, maxPossibleGas);
-        require(approvalData.length == 0, "approvalData: invalid length");
-        require(relayRequest.relayData.paymasterData.length == 0, "paymasterData: invalid length");
+        require(approvalData.length == 0, ERROR_APPROVAL_DATA_LENGTH_INVALID);
+        require(relayRequest.relayData.paymasterData.length == 0, ERROR_APPROVAL_DATA_LENGTH_INVALID);
 
-        if ( useSenderWhitelist ) {
-            require( senderWhitelist[relayRequest.request.from], "sender not whitelisted");
-        }
-        if ( useTargetWhitelist ) {
-            require( targetWhitelist[relayRequest.request.to], "target not whitelisted");
-        }
+        require(
+            willPerform(
+                relayRequest.request.to,
+                relayRequest.request.from,
+                PAYMASTER_SPONSORED_ROLE,
+                relayRequest.relayData.paymasterData
+            ),
+            ERROR_NOT_SPONSORED
+        );
+
         return ("", false);
     }
 }
