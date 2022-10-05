f86813e7cde9c26daab12415d47b632cb124dfe3
sepehr sanaee <sepehr2mail@gmail.com>
2022-10-05 12:44:58 +0330

    create a component for protectedRoute

---
+++ packages/web-app/src/components/protectedRoute/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Outlet} from 'react-router-dom';
+
+import {useGlobalModalContext} from 'context/globalModals';
+import {useDaoDetails} from 'hooks/useDaoDetails';
+import {useDaoParam} from 'hooks/useDaoParam';
+import {useWallet} from 'hooks/useWallet';
+import {useDaoMembers} from 'hooks/useDaoMembers';
+import {PluginTypes} from 'hooks/usePluginClient';
+import {Loading} from 'components/temporary';
+
+const ProtectedRoute: React.FC = () => {
+  const {data: dao, isLoading: paramIsLoading} = useDaoParam();
+  const {address} = useWallet();
+  const {data: daoDetails, isLoading: detailsAreLoading} = useDaoDetails(
+    dao || ''
+  );
+  const {open} = useGlobalModalContext();
+  const {
+    data: {filteredMembers},
+    isLoading: MembershipIsLoading,
+  } = useDaoMembers(
+    daoDetails?.plugins[0].instanceAddress || '',
+    daoDetails?.plugins[0].id as PluginTypes,
+    address as string
+  );
+
+  if (paramIsLoading || detailsAreLoading || MembershipIsLoading)
+    return <Loading />;
+
+  if (filteredMembers.length === 0 && daoDetails)
+    open(
+      daoDetails?.plugins[0].id === 'erc20voting.dao.eth'
+        ? 'requiredToken'
+        : 'requiredWallet'
+    );
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
