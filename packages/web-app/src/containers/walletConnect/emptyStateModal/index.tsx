65933ecc503a25f57965816b75abb95a28217bc9
Fabrice Francois <fabricevladimir@gmail.com>
2023-06-25 13:47:49 -0400

    extend StateEmpty component

--- packages/web-app/src/containers/walletConnect/emptyStateModal/index.tsx
+++ packages/web-app/src/containers/walletConnect/emptyStateModal/index.tsx
@@ -1,11 +1,11 @@
-import {ButtonText} from '@aragon/ui-components';
-import ModalBottomSheetSwitcher from 'components/modalBottomSheetSwitcher';
-import WalletConnectSVG from 'public/walletConnect.svg';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
 
+import WalletConnectSVG from 'public/walletConnect.svg';
+import ModalBottomSheetSwitcher from 'components/modalBottomSheetSwitcher';
 import ModalHeader from 'components/modalHeader';
+import {StateEmpty} from 'components/stateEmpty';
 import useScreen from 'hooks/useScreen';
 import {htmlIn} from 'utils/htmlIn';
 
@@ -24,31 +24,23 @@ const EmptyState: React.FC<Props> = props => {
     <ModalBottomSheetSwitcher isOpen={props.isOpen} onClose={props.onClose}>
       <ModalHeader
         title={t('wc.emptyState.modalTitle')}
-        onClose={props.onClose}
         showBackButton
         onBackButtonClicked={props.onBackButtonClicked}
-        {...(isDesktop ? {showCloseButton: true} : {})}
+        {...(isDesktop ? {showCloseButton: true, onClose: props.onClose} : {})}
       />
       <Content>
-        <div className="flex justify-center items-center h-14 desktop:h-20">
-          <img src={WalletConnectSVG} />
-        </div>
-        <ContentWrapper>
-          <TextWrapper>
-            <Title>{t('wc.emptyState.title')}</Title>
-            <Description
-              dangerouslySetInnerHTML={{
-                __html: htmlIn(t)('wc.emptyState.desc'),
-              }}
-            />
-          </TextWrapper>
-          <ButtonText
-            label={t('wc.emptyState.ctaLabel')}
-            mode="primary"
-            size="large"
-            className="w-full"
-          />
-        </ContentWrapper>
+        <StateEmpty
+          type="custom"
+          src={WalletConnectSVG}
+          mode="inline"
+          title={t('wc.emptyState.title')}
+          description={htmlIn(t)('wc.emptyState.desc')}
+          renderHtml
+          primaryButton={{
+            label: t('wc.emptyState.ctaLabel'),
+            onClick: props.onCtaClicked,
+          }}
+        />
       </Content>
     </ModalBottomSheetSwitcher>
   );
@@ -59,21 +51,3 @@ export default EmptyState;
 const Content = styled.div.attrs({
   className: 'px-2 tablet:px-3 pb-3',
 })``;
-
-const ContentWrapper = styled.div.attrs({className: 'space-y-3 w-full'})``;
-
-const TextWrapper = styled.div.attrs({
-  className: 'space-y-1.5 text-center',
-})``;
-
-const Title = styled.h2.attrs({
-  className: 'ft-text-xl font-bold text-ui-800',
-})``;
-
-const Description = styled.p.attrs({
-  className: 'text-ui-500 ft-text-sm tablet:ft-text-base',
-})`
-  & > a {
-    color: #003bf5;
-    font-weight: 700;
-`;
